fc3c97829268e3b45e80bf44d85ebabbf2c42314
Kevin <scheeren@proton.me>
2022-10-26 00:35:32 +0200

    Created Link Page

---
+++ components/link.vue
@@ -0,0 +1,20 @@
+<template>
+  <a
+    :href="url"
+    data-aos="fade-up"
+    :data-aos-delay="delay"
+    class="flex items-center justify-center w-5/6 max-w-sm h-14 cursor-pointer p-3 m-3 mx-auto rounded-md ring-[1px] ring-green-600 backdrop-filter backdrop-blur-2xl bg-green-300 hover:bg-green-400 shadow-xl"
+  >
+    <Icon :name="icon" class="text-2xl text-black" />
+    <h2 class="ml-2">{{ name }}</h2>
+  </a>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  url: String,
+  icon: String,
+  name: String,
+  delay: Number,
+});
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,63 @@
 <template>
-  <div>
-    <h1 class="text-5xl text-red-600 text-center mt-12">Hello World!</h1>
+  <div
+    class="flex flex-col items-center justify-center min-h-screen min-w-screen bg-gray-900 overflow-hidden pb-32"
+  >
+    <div
+      class="h-16 md:h-24 lg:h-32 my-4"
+      data-aos="fade-down"
+      data-aos-delay="300"
+    >
+      <img src="/img/logo.png" class="h-16 md:h-24 lg:h-32" />
+    </div>
+    <Link
+      v-for="(link, index) in links"
+      :url="link.url"
+      :name="link.name"
+      :icon="link.icon"
+      :delay="index * 100"
+    />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
+
+<script setup lang="ts">
+const links = [
+  {
+    url: "https://rejuvenatefinance.com",
+    icon: "mdi:web",
+    name: "Website",
+  },
+  {
+    url: "https://discord.com/invite/Cf2VUn4e2R",
+    icon: "mdi:discord",
+    name: "Discord",
+  },
+  {
+    url: "https://t.me/+LxJTReW_wDRmMmYx",
+    icon: "mdi:telegram",
+    name: "Telegram",
+  },
+  {
+    url: "https://twitter.com/rejuvenaterjv",
+    icon: "mdi:twitter",
+    name: "Twitter",
+  },
+  {
+    url: "https://app.rejuvenatefinance.com",
+    icon: "mdi:open-in-app",
+    name: "DApp",
+  },
+  {
+    url: "https://github.com/rejuvenatefinance",
+    icon: "mdi:github",
+    name: "GitHub",
+  },
+];
+</script>
